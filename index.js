22d464f596d693599ddbd49dba9eae206a85aad6
MaxAshby <movementmylife@gmail.com>
2021-02-21 12:24:39 +0300

    update index.js and install prettier

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import _ from "lodash"
+import _ from "lodash";
 
-console.log(_.last(['one', 'two']));
-console.log('Hello, Hexlet')
+console.log(_.last(["one", "two"]));
+console.log("Hello, Hexlet");
+
+for (let i = 0; i <= 100; i += 1){
+    console.log(i); console.log('he'); i++; // hello
+}
